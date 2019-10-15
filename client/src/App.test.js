29e4f7a4a7c8b51149deb49f1119bf961da0c650
JoeLorenzo <Joselmedrano@gmail.com>
2019-10-15 17:21:48 -0500

    -added testing

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,12 +1,25 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 import App from './App';
 import * as rtl from '@testing-library/react';
-   import '@testing-library/jest-dom/extend-expect';
+import '@testing-library/jest-dom/extend-expect';
+import Player from './components/Player'
+import ApiCall from './components/ApiCall'
+import Online from './components/Online'
+import { useOnline } from './hooks/useOnline'
+import { useLocalStorage, useInput } from './hooks/useLocalStorage'
+afterEach(rtl.cleanup);
 
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+test('renders App component without crashing', () => {
+const wrapper = rtl.render(<App/>)
+  wrapper.debug()
+});
+  test('renders Player component without crashing', () => {
+  const wrapper = rtl.render(<ApiCall/>)
+    wrapper.debug()
+});
+
+  test('renders Player component without crashing', () => {
+  const wrapper = rtl.render(<useLocalStorage/>)
+    wrapper.debug()
 });
